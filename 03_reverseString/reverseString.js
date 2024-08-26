acd964c74b03e642dfa7c2db601229a75211ebc6
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-26 12:29:10 -0400

    use better built in methods for reversing the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,5 @@
 const reverseString = function(string) {
-    output = "";
-    for (let i = string.length-1; i >= 0; i--){
-        output += string[i];
-    }
-    return output;
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
