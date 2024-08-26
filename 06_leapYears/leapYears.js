6213dfbf26233418695845a3a0b48062b5a4aa14
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-26 13:36:20 -0400

    add work for exercise 6 with clean code

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isDivisibleByFour = year % 4 === 0;
+    let isNotCentury = year % 100 !== 0;
+    let isDivisibleBy400 = year % 400 === 0;
+    if (isDivisibleByFour && isNotCentury || isDivisibleBy400){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
