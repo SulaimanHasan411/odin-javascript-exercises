c5fc524096c28bf166f51adfaa3812ed53375edb
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-30 23:04:06 -0400

    add work for exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    alphaNumeric = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    const newStr = str
+     .toLowerCase()
+     .split("")
+     .filter((char) => alphaNumeric.includes(char))
+     .join("");
+
+    rev = newStr.split("").reverse().join("");
+
+    return rev === newStr;
+
 
 };
 
