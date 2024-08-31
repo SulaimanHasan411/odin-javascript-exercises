42e4fb0f6007bdb6730e6359b140705117c2a8a1
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-30 23:45:58 -0400

    add work for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce(function (oldestSoFar, currentPerson){
+        let oldestAge;
+        if ("yearOfDeath" in oldestSoFar) {
+            oldestAge = oldestSoFar.yearOfDeath - oldestSoFar.yearOfBirth;
+        }
+        else {
+            oldestAge = (new Date).getFullYear() - oldestSoFar.yearOfBirth;
+        }
 
+        let currentAge;
+        if ("yearOfDeath" in currentPerson) {
+            currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        }
+        else {
+            currentAge = (new Date).getFullYear() - currentPerson.yearOfBirth;
+        }
+        if (oldestAge > currentAge){
+            return oldestSoFar;
+        }
+        else {
+            return currentPerson;
+        }
+    });
 };
 
 // Do not edit below this line
