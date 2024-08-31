6ee8755987f2da4fe517a73d7b535dc66ea3f532
Sulaiman <SulaimanHasan411@gmail.com>
2024-08-30 23:16:48 -0400

    add work for exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num !== "number"){
+        num = parseInt(num);
+    }
+    if (num < 0){
+        return "OOPS";
+    }
+    if (num === 0) {
+        return 0;
+    }
 
+    //first and second values r 1, so loop doesnt need to execute
+    prev1 = 1;
+    prev2 = 1;
+    for (let i = 3; i <= num; i++){
+        temp = prev1;
+        prev1 += prev2;
+        prev2 = temp;
+    }
+    return prev1;
 };
 
 // Do not edit below this line
